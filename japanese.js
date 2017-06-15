acb250c5424235c639c7f7b8b1dec37bdba3570b
Chris Mellor <chrismellor@outlook.com>
2017-06-14 22:42:21 -0500

    made the html and css file. added dummy javascript to japanese.js. created the bones for the projct and styled it to make it an easy and pleasant user experience.

---
+++ japanese.js
@@ -0,0 +1,6 @@
+$("#submit").click(function(){
+	$(".translatedText").html(`<h1 id="translatedText" class="text-center">Happy International Yoga Day!
+Your holiday is dumb. Have a healthy year!
+Sincerely - 'your name'`);
+
+});
