76c6e69e975abdb4d8c0002e650abbeff4d4a74e
Arwa <arwa.it15@gmail.com>
2017-06-15 08:44:43 -0500

    object for french.js

---
+++ french.js
@@ -0,0 +1,29 @@
+var Translate = (function(oldTranslate)
+{
+
+	oldTranslate.french = {
+		"hello" : "bonjour",
+		"happy" : "joyeux",
+		"yoga" : "yoga",
+		"international" : "international",
+		"your" : "votre",
+		"holiday" : "vacances",
+		"is" : "est",
+		"dumb" : "stupide",
+		"hope" : "esperer",
+		"you" : "vous",
+		"have" : "avoir",
+		"a" : "un",
+		"healthy" : "portant",
+		"very" : "bein",
+		"year" : "an",
+		"ahead" : "devant",
+		"yours" : "tienne",
+		"truly" : "sincerement",
+		"sincerely" : "cordialement",
+		"friend" : "ami",
+		"love" : "amour"
+	}
+
+	return oldTranslate;
+}(Translate))
---
+++ main.js
@@ -0,0 +1,14 @@
+// let translateBtn = document.getElementById('translateBtn'); 
+
+// translateBtn.addEventListener("click", function()
+// {
+// 	let msgInEnglish = document.getElementById('textarea').value;
+// 	let selectedDropDown = document.getElementById('dropDown').selected;
+// 	msgArray = msgInEnglish.split(", ");
+// 	if(selectedDropDown.value === 'french')
+// 		{
+// 			Translate.frenchTranslation();
+// 		}
+// })
+var Translate = {};
+
